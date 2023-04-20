f136a6eb73af73249466aa1b28c278cccf64ccf4
TimFB <timf.brueggemann1999@gmx.de>
2023-04-20 20:04:59 +0200

    fix .gitignore and upload some stuff

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = '6d388933'
+version = '0.1.0+dev.6d388933'
+linkified_version = '0.1.0+dev.[6d388933](https://github.com/maunium/stickerpicker/commit/6d388933cfdad3729874207da378da2336804793)'
